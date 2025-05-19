8f73166a0b92504005c8d44e6148e70e3789d541
Malik <mmalith520@gmail.com>
2025-05-19 17:01:21 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import Hero from './components/Hero.jsx'
+function App() {
+  return (
+    <>
+      <main className='relative min-h-screen w-screen overflow-hidden-x-hidden '>
+        <Hero />
+      </main>
+    </>
+  )
+}
+
+export default App
