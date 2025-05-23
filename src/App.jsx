7ba2d5077c1829eb6a4a30eee543c004f65250d1
MalikBandara <mmalith520@gmail.com>
2025-05-23 12:33:49 +0530

    navbar done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import Hero from './components/Hero.jsx'
 import About from './components/About.jsx'
+import Navbar from './components/Navbar.jsx'
 function App() {
   return (
     <>
-      <main className='relative min-h-screen w-screen overflow-hidden-x-hidden '>
+      <main className='relative min-h-screen w-screen overflow-hidden-x-hidden  '>
+        <Navbar/>
         <Hero />
         <About/>
       </main>
