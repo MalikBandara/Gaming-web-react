bbb0e594ea43d6f4cfec6da4e259c628d2dfc298
MalikBandara <mmalith520@gmail.com>
2025-05-23 16:03:55 +0530

    footer done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
+import { FaGithub, FaLinkedinIn } from 'react-icons/fa'
+
+const links = [
+  { href: 'https://github.com/MalikBandara', icon: <FaGithub /> },
+  {
+    href: 'https://www.linkedin.com/in/malikbandara?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
+    icon: <FaLinkedinIn />,
+  },
+]
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="w-screen bg-violet-300 py-4 text-black ">
+      <div className="container mx-auto flex flex-col items-center justify-between gap-4 px-4 md:flex-row">
+        <p className="text-center text-sm md:text-left ">
+          &copy;Malik Bandara 2025 All rights reserved
+        </p>
+        <div className="flex justify-center gap-4 md:justify-start ">
+          {links.map(link => (
+            <a
+              key={link}
+              href={link.href}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="transition-colors duration-500 ease-in-out hover:text-white  text-black"
+            >
+              {link.icon}
+            </a>
+          ))}
+        </div>
+        <a href="#privacy-policy" className='text-center text-sm hover:underline md:text-right'>
+            Privacy Policy
+        </a>
+      </div>
+    </footer>
   )
 }
 
